84c8a8bd22b9761672462bc3ad157c7a67f3ed44
Lyle Dippo <ldippo421@gmail.com>
2014-10-08 17:05:46 -0400

    added form group helper, discovered bug with user logged in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
 
 
-  def show
-  	@post = Post.find(params[:id])
+   def show
     @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
   end
 
   def new
