0c800658fe3e48c8f2af88ac8ceb5b72cb446cd4
Lyle Dippo <ldippo421@gmail.com>
2014-10-10 16:27:47 -0400

    created destroy actions

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -9,6 +9,20 @@ class TopicsController < ApplicationController
      authorize @topic
   end
 
+   def destroy
+     @topic = Topic.find(params[:id])
+     name = @topic.name
+ 
+     authorize @topic
+     if @topic.destroy
+       flash[:notice] = "\"#{name}\" was deleted successfully."
+       redirect_to topics_path
+     else
+       flash[:error] = "There was an error deleting the topic."
+       render :show
+     end
+   end
+
   def show
      @topic = Topic.find(params[:id])
      @posts = @topic.posts.paginate(page: params[:page], per_page: 10)
